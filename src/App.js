8177b063a8274d87f2172db528f86262a73d5609
Mihut Marius <yymihut@gmail.com>
2020-06-18 20:17:21 +0300

    styled-comp package added

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import MyImdb from '../src/Containers/MyImdb';
 import './App.css';
 
 
--- src/Containers/MyImdb.js
+++ src/Containers/MyImdb.js
@@ -1,2 +1,18 @@
 //header + HomePage (dinamic-component + movie details)
 //contine zona de switch cu route - pt schimbarea paginilor 
+
+import React, { Component } from 'react';
+import './MyImdb.css';
+
+
+class MyImdb extends Component {
+  render() {
+    return (
+          <div>My imdb</div>       
+    );
+  }
+}
+
+export default MyImdb
+
+
