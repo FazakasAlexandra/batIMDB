8007584b48adce344d80332e64190f2dab1cfdb4
mandereea <60708962+mandereea@users.noreply.github.com>
2020-06-19 13:16:20 +0300

    MovieCard draft

---
+++ src/Components/MovieCard/MovieCard.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import './MovieCard.css'
+class MovieCard extends React.Component{
+    constructor(props){
+        super(props)
+    }
+    render(){
+        const { poster, title, imdbRating } = this.props
+        return (
+            <div className='movieCard'>
+                <img src={ poster } className='cardImg' />
+                <p className='cardTitle'>{ title }</p>
+                <p className='cardRating'>RATING: { imdbRating }</p>
+                <div className='nextLevel'></div>
+                <button className ='movieDetailsBtn'>VIEW </button>
+            </div>
+        )
+    }
+}
+export {MovieCard}
