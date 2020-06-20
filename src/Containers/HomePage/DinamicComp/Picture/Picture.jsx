9c217467a066065f9f235ab52e67cc91c882f2a5
Mihut Marius <yymihut@gmail.com>
2020-06-21 01:22:34 +0300

    dinamic-comp update

---
+++ src/Containers/HomePage/DinamicComp/Picture/Picture.jsx
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import RotateList from 'react-rotate-list';
+import '../Picture/Picture.css';
+
+class Picture extends Component {
+    render() {        
+        const { picturesArray } = this.props;
+        let pics = picturesArray.map((pic, i) => {
+            return (
+                <img src={pic} key={i}/>
+            )
+        });
+        return (
+        <div className="Picture">
+            <RotateList height={580} duration={900} delay={3000} >
+              {pics}
+            </RotateList>
+        </div>            
+            
+        )
+    }
+}
+
+export default Picture;
