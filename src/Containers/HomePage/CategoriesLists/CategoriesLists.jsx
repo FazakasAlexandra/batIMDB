cfbecc2ed3eed64dd48833552cfc16e05afd4577
Daniel <daniel.selistean@yahoo.com>
2020-06-21 22:39:13 +0300

    Categories Lists mouvies

---
+++ src/Containers/HomePage/CategoriesLists/CategoriesLists.jsx
@@ -0,0 +1,104 @@
+import React from 'react'
+import MovieCard from '../../../Components/MovieCard/MovieCard'
+import axios from 'axios'
+import './CategoriesLists.css'
+
+ class CategoriesLists extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            movieListMovies: [],
+            seriesListMovies: [],
+            gameListMovies: []
+        }
+    }
+
+    componentDidMount() {
+        this.getMovieListMovies();
+        this.getSeriesListMovies();
+        this.getGameListMovies();
+    }
+
+    getMovieListMovies = () => {
+        axios.get(`http://ancient-caverns-16784.herokuapp.com/movies?Type=movie&take=6`)
+            .then((response) => {
+                console.log(response.data.results)
+                this.setState({ movieListMovies: response.data.results })
+            }
+            )
+    }
+    getSeriesListMovies = () => {
+        axios.get(`http://ancient-caverns-16784.herokuapp.com/movies?Type=series&take=6`)
+            .then((response) => {
+                console.log(response.data.results)
+                this.setState({ seriesListMovies: response.data.results })
+            }
+            )
+    }
+    getGameListMovies = () => {
+        axios.get(`http://ancient-caverns-16784.herokuapp.com/movies?Type=game&take=6`)
+            .then((response) => {
+                console.log(response.data.results)
+                this.setState({ gameListMovies: response.data.results })
+            }
+            )
+    }
+    displayMovie() {
+        let { movieListMovies } = this.state
+        let movies = movieListMovies.map(movie => {
+            return (<MovieCard
+                key={movie._id}
+                poster={movie.Poster}
+                title={movie.Title}
+                imdbRating={movie.imdbRating}
+            />)
+        })
+        return movies;
+    }
+    displaySerie() {
+        let { seriesListMovies } = this.state
+        let series = seriesListMovies.map(movie => {
+            return (<MovieCard
+                key={movie._id}
+                poster={movie.Poster}
+                title={movie.Title}
+                imdbRating={movie.imdbRating}
+            />)
+        })
+        return series;
+    }
+    displayGame() {
+        let { gameListMovies } = this.state
+        let games = gameListMovies.map(movie => {
+            return (<MovieCard
+                key={movie._id}
+                poster={movie.Poster}
+                title={movie.Title}
+                imdbRating={movie.imdbRating}
+            />)
+        })
+        return games;
+    }
+
+
+    render() {
+        return (
+            <div className="container">
+                <h2>Category: Movies</h2>
+                <div className="categoriesList movieListMovies ">
+                    {this.displayMovie()}
+                </div>
+                <h2>Category: Series</h2>
+                <div className="categoriesList seriesListMovies">
+                    {this.displaySerie()}
+                </div>
+                <h2>Category: Game</h2>
+                <div className="categoriesList gameListMovies">
+                    {this.displayGame()}
+                </div>
+            </div>
+        )
+    }
+}
+
+export default CategoriesLists
--- src/Containers/HomePage/HomePage.jsx
+++ src/Containers/HomePage/HomePage.jsx
@@ -2,12 +2,14 @@ import React, { Component } from 'react';
 import { withRouter } from 'react-router-dom';
 import DinamicComp from '../HomePage/DinamicComp/DinamicComp';
 import './HomePage.css';
+import CategoriesLists from '../HomePage/CategoriesLists/CategoriesLists'
 
 class HomePage extends Component {
     render() {
         return (
             <div className="Hompage">
                 <DinamicComp/>  //componenta Marius 
+                <CategoriesLists />
             /* CategoriesList component */   //componenta Daniel 
             </div>
         );
