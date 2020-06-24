0b1e03f04f14cfa03e907f5edbcf534d440186a6
Marius Mihut <mariusm@CSiRO.local>
2020-06-24 16:23:35 +0300

    resp

--- src/Containers/HomePage/DinamicComp/RespPlayer/RespPlayer.jsx
+++ src/Containers/HomePage/DinamicComp/RespPlayer/RespPlayer.jsx
@@ -1,17 +1,21 @@
 import React, { Component } from 'react';
-import ReactPlayer from 'react-player'
 
 class RespPlayer extends Component {
     render() {
         return (
-            <div className='player-wrapper'>
-                <ReactPlayer
-                    className='react-player'
-                    url={this.props.url}
-                    width='100%'
-                    height='100%'
-                />
-            </div>
+            <iframe
+                // vine pe resp la al doilea fetch
+                // response.data.linkEmbed  "https://www.imdb.com/video/imdb/vi1946858521/imdb/embed?autoplay=false&width=480"
+                /* src="https://www.imdb.com/video/imdb/vi1946858521/imdb/embed?autoplay=false&width=480" */
+                src={`${this.props.url}?autoplay=false&width=480`}
+                width="480"
+                height="270"
+                allowfullscreen="true"
+                mozallowfullscreen="true"
+                webkitallowfullscreen="true"
+                frameborder="no"
+                scrolling="no">
+            </iframe>
         )
     }
 }
