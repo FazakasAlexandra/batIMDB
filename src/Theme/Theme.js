06fd769ad47051ccbc3f42d8f245a0c71a6128a4
Fazakas Alexandra <alexandra.fazakas91@gmail.com>
2020-07-02 11:44:04 +0300

    merge

--- src/Theme/Theme.js
+++ src/Theme/Theme.js
@@ -12,13 +12,8 @@ export const themeLight = {
     },
     backgroundMenu: {
         backgroundColor: "#282e3c",
-<<<<<<< HEAD
-    }
-
-=======
     },
     frame: require('../Images/frameBlack1.png')
->>>>>>> 23bcceedb8d56c381a95c81496d342339470e458
 };
 
 export const themeDark = {
@@ -32,15 +27,11 @@ export const themeDark = {
     },
     shadows: {
         boxShadow1: "0px 5px 15px 8px rgba(255,255,255,1)"
-<<<<<<< HEAD
-    }
-=======
     },
     backgroundMenu: {
         backgroundColor: "#282e3c",
     },
     frame: require('../Images/frameYellow.png')
->>>>>>> 23bcceedb8d56c381a95c81496d342339470e458
 };
 
 
