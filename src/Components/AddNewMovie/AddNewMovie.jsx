7dca6ae8dc8b73a337de4a921f113135fcc3a0db
Marius Mihut <mariusm@CSiRO.local>
2020-07-06 11:42:33 +0300

    theme with object

--- src/Components/AddNewMovie/AddNewMovie.jsx
+++ src/Components/AddNewMovie/AddNewMovie.jsx
@@ -76,7 +76,7 @@ class AddNewMovie extends React.Component {
     }
     //creates preview section (label + text)
     renderPreview(value, labelName) {
-        const { theme } = this.props;
+        const { theme } = this.props.theme.type;
         return (
             <p className={createClassName(theme, 'pvwLine')}>
                 <span className={createClassName(theme)}
